961a6311b1255630aa3f35cb9c9253ed42829a75
Juan Camilo Daza <jc.dazam1@uniandes.edu.co>
2022-09-20 21:57:56 -0500

    Seguridad restaurante controller: OK

--- src/restaurante/restaurante.controller.ts
+++ src/restaurante/restaurante.controller.ts
@@ -1,40 +1,50 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseGuards, UseInterceptors } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Role } from 'src/usuario/role.enum';
 import { plainToInstance } from 'class-transformer';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
 import { RestauranteDto } from './restaurante.dto';
 import { RestauranteEntity } from './restaurante.entity';
 import { RestauranteService } from './restaurante.service';
+import { HasRoles } from 'src/usuario/roles.decorator';
 
 @Controller('restaurante')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class RestauranteController {
     constructor(private readonly restauranteService: RestauranteService) {}
-
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Get()
-    async findAll() {
+    @HasRoles(Role.Lector, Role.Admin, Role.LectorRestaurante)
+    async findAll(): Promise<RestauranteEntity[]> {
       return this.restauranteService.findAll();
     }
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Get(':restauranteId')
-    async findOne(@Param('restauranteId') restauranteId: string) {
+    @HasRoles(Role.Lector, Role.Admin, Role.LectorRestaurante)
+    async findOne(@Param('restauranteId') restauranteId: string): Promise<RestauranteEntity> {
         return this.restauranteService.findOne(restauranteId);
     }
-
-    
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Post()
-    async create(@Body() restauranteDto: RestauranteDto) {
+    @HasRoles(Role.Editor, Role.Admin, Role.EditorRestaurante)
+    @Post()
+    async create(@Body() restauranteDto: RestauranteDto): Promise<RestauranteEntity> {
         const restaurante: RestauranteEntity = plainToInstance(RestauranteEntity, restauranteDto);
         return this.restauranteService.create(restaurante);
     }
-
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Put(':restauranteId')
-    async update(@Param('restauranteId') restauranteId: string, @Body() restauranteDto: RestauranteDto) {
+    @HasRoles(Role.Editor, Role.Admin, Role.EditorRestaurante)
+    async update(@Param('restauranteId') restauranteId: string, @Body() restauranteDto: RestauranteDto): Promise<RestauranteEntity> {
         const restaurante: RestauranteEntity = plainToInstance(RestauranteEntity, restauranteDto);
         return this.restauranteService.update(restauranteId, restaurante);
     }
-
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Delete(':restauranteId')
+    @HasRoles(Role.Borrar, Role.Admin, Role.BorrarRestaurante)
     @HttpCode(204)
-    async delete(@Param('restauranteId') restauranteId: string) {
+    async delete(@Param('restauranteId') restauranteId: string): Promise<void> {
         return this.restauranteService.delete(restauranteId);
     }
 
