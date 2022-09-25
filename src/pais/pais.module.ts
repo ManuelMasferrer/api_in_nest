3e1b87feec5b51f650c426aace712dd2fd3773a8
Manuel Sanchez Masferrer <ma.sanchezm12@uniandes.edu.co>
2022-09-25 11:30:30 -0600

    Update pais.module.ts

--- src/pais/pais.module.ts
+++ src/pais/pais.module.ts
@@ -1,12 +1,11 @@
 import { CacheModule, Module } from '@nestjs/common';
-import { PaisController } from './pais.controller';
-import { PaisService } from './pais.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PaisEntity } from './pais.entity';
-
+ import { PaisController } from './pais.controller';
+ import { PaisService } from './pais.service';
+ import { TypeOrmModule } from '@nestjs/typeorm';
+ import { PaisEntity } from './pais.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PaisEntity])],
+  imports: [TypeOrmModule.forFeature([PaisEntity]), CacheModule.register()],
   controllers: [PaisController], 
   providers: [PaisService,]
 })
