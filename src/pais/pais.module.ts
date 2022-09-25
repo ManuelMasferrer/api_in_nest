0e210ab207f3c28f984b2edc09dbef41d729597d
Manuel Sanchez Masferrer <ma.sanchezm12@uniandes.edu.co>
2022-09-25 11:20:23 -0600

    fixed unit tests with cache

--- src/pais/pais.module.ts
+++ src/pais/pais.module.ts
@@ -1,8 +1,8 @@
 import { CacheModule, Module } from '@nestjs/common';
-import { PaisController } from './pais.controller';
-import { PaisService } from './pais.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PaisEntity } from './pais.entity';
+ import { PaisController } from './pais.controller';
+ import { PaisService } from './pais.service';
+ import { TypeOrmModule } from '@nestjs/typeorm';
+ import { PaisEntity } from './pais.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PaisEntity]), CacheModule.register()],
