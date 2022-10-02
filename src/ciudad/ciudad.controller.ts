f0323d87f04862bf64c2af9c036483a2f92e869a
farojasp1 <fa.rojasp1@uniandes.edu.co>
2022-10-02 13:30:13 -0500

    segurida ciudad

--- src/ciudad/ciudad.controller.ts
+++ src/ciudad/ciudad.controller.ts
@@ -1,24 +1,30 @@
 
 
-import { Body, Controller, Delete, Get, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards, UseInterceptors } from '@nestjs/common';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors.interceptor';
-import { CiudadDto } from './ciudad.dto';
-import { CiudadEntity } from './ciudad.entity';
 import { CiudadService } from './ciudad.service';
 import { plainToInstance } from 'class-transformer';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { HasRoles } from 'src/usuario/roles.decorator';
+import { Role } from 'src/usuario/role.enum';
 
 
 @Controller('ciudad')
 @UseInterceptors(BusinessErrorsInterceptor)
 export class CiudadController {
     constructor(private readonly ciudadService: CiudadService) {}
-
+    
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Get()
+    @HasRoles(Role.Lector, Role.Admin, Role.LectorCiudad)    
     async findAll() {
         await this.ciudadService.findAll();
     }
 
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Get(':CiudadId')
+    @HasRoles(Role.Lector, Role.Admin, Role.LectorCiudad)
     async findOne(@Param('CiudadId') CiudadId: string){
         return await this.ciudadService.findOne(CiudadId);
     }
